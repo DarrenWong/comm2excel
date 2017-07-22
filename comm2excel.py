a3a1f22561ccf11303f20b944f3193ef74eee1f0
Darren Wong <darrenwongf@gmail.com>
2017-07-22 13:16:13 +0800

    Update comm2excel.py

--- comm2excel.py
+++ comm2excel.py
@@ -7,7 +7,7 @@ ser = serial.Serial('COM1',9600,timeout=1)
 while True:
   commdata = ser.readline()
   if(commdata):
-  	result = re.sub('[^0-9\.]', '', commdata) #filter for number and dot only
-  	pyautogui.typewrite(result)
-  	pyautogui.press('enter')
+    result = re.sub('[^0-9\.]', '', commdata) #filter for number and dot only
+    pyautogui.typewrite(result)
+    pyautogui.press('enter')
 
